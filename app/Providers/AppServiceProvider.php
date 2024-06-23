24c78ef1d9b9cd41e51fd4beba0ea92cac14b521
ayubxon <ayubxonnematov@gmail.com>
2024-06-23 16:58:10 +0500

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('viewApiDocs', function ($user = null) {
+            return true;
+        });
 
+        Gate::define('viewTelescope', function ($user = null) {
+            return true;
+        });
     }
 }
